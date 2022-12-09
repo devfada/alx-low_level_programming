5afca0b3b21f9d2fb6b8e18175547a05db17ebe1
devfada <fadamullahemmanuel@gmail.com>
2022-12-09 05:39:34 -0800

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void)/*c program puts*/
 {
-char MyStr[] = "Programming is like building a multilingual puzzle";
+char MyStr[] = "\"Programming is like building a multilingual puzzle";
 puts(MyStr);
 return (0);
 }
