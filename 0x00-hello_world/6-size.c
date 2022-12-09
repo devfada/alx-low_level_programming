8beb0aa06fd9dded71e3b854d0f24a958404f666
devfada <fadamullahemmanuel@gmail.com>
2022-12-09 06:15:25 -0800

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+char charType;
+int intType;
+long int lintType;
+long long int llintType;
+float floatType;
+printf("Size of a char:%zu bytes\n",sizeof(charType));
+printf("Size of an int:%zu bytes\n",sizeof(intType));
+printf("Size of a long int:%zu bytes\n",sizeof(lintType));
+printf("Size of a long long int:%zu bytes\n",sizeof(llintType));
+printf("Size of a float:%zu bytes\n",sizeof(floatType));
+return (0);
+}
