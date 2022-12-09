4332834c78cb0e09ad8ab7118c31571ea11c5aaa
devfada <fadamullahemmanuel@gmail.com>
2022-12-09 05:03:34 -0800

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int main(void)
 {
-	char MyStr[]="Programming is like building a multilingual puzzle"
-	puts(MyStr);
-	return 0;
+    char MyStr[]="Programming is like building a multilingual puzzle";
+    puts(MyStr);
+    return 0;
 }
