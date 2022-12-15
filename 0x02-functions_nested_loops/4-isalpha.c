e97e3b00034c2e1ddf770f1113ca50c0a03b5358
devfada <fadamullahemmanuel@gmail.com>
2022-12-14 23:54:17 -0800

    _isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha-
+ *
+ * Return- if uppercase or lowercase return 1 otherwise 0
+ */
+int _isalpha(int c)
+{
+if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
