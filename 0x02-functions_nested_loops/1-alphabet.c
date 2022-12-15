a257385facd6c97391630d75d09bb6ae881abbd3
devfada <fadamullahemmanuel@gmail.com>
2022-12-14 22:25:41 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,9 +4,12 @@
  *
  * Return: Always 0
  */
-int main(void)
+void print_alphabet(void)
 {
-_putchar(print_alphabet);
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
 _putchar('\n');
-return (0);
 }
