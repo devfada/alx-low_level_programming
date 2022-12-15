63ea3377253252632b45efcced7aff5dec3c53a6
devfada <fadamullahemmanuel@gmail.com>
2022-12-14 23:23:39 -0800

    _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower-check for lower case characters
+ *
+ * Return: 1 if lowercase 0 otherwise
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
