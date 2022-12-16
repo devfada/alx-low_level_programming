fb331d024672f67f299307721079c27cf2ba394b
devfada <fadamullahemmanuel@gmail.com>
2022-12-16 00:17:25 -0800

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_sign-print the sign
+ *
+ *Return: 1, 0, -1
+ */
+int print_sign(int n)
+{
+if (n>0)
+{
+printf("+, %d", 1);
+}
+else if (n==0)
+{
+printf("0, %d", 0);
+}
+else
+printf("-, %d", -1);
+}
