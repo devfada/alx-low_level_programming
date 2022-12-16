1e3da31a98ffb00b9b61a3f63f77c6bc595b311c
devfada <fadamullahemmanuel@gmail.com>
2022-12-16 01:05:18 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+ *_abs-function that gives an absolute value
+ *int-value to be made absolute
+ *Return: positive value
+ */
+int _abs(int)
+{
+if (int < 0)
+{
+int = (-1) * int;
+}
+_putchar(int);
+else
+_putchar(int);
+}
