f4d8353fcc9407c4b1821c4afb63f603e6570149
devfada <fadamullahemmanuel@gmail.com>
2022-12-20 04:24:27 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ *reset_to_98 - function does not modify p 
+ *@int * n: a pointer variable
+ *Return: nothing
+ */
+void reset_to_98(int *n)
+{
+n = &n;
+*n = 98;
+}
+/**
+ *main - parameters are passed by value
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+int n = 402;
+_putchar("n=%d\n",n)
+reset_to_98(n);
+_putchar("Value of n is:%d\n",n);
+return (0);
+}
+
