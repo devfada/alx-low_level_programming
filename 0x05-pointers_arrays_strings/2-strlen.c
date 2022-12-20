dc407d439a316ef37bec5e448ad790201cef9eb6
devfada <fadamullahemmanuel@gmail.com>
2022-12-20 06:19:34 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_strlen- function to return length
+ *Return: length
+ */
+int _strlen(char *s) 
+{
+int length = 0;
+while (*s != '\0') 
+{
+length++;
+s++;
+}
+return length;
+}
