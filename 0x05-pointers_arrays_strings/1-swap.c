6457d12a0267b5fda1ec4aa9b8c5706df87a8847
devfada <fadamullahemmanuel@gmail.com>
2023-01-04 06:30:03 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,25 +1,11 @@
 #include "main.h"
 /**
- *swap_int- function to swap values
- *
- *Return: nothing
+ * reset_to_98 - a function that takes a pointer to an int
+ * and updates the value it points to 98
+ * @n: input
+ * Return: n
  */
-void swap_int(int *a, int *b)
+void reset_to_98(int *n)
 {
-int temp = *a;
-*a = *b;
-*b = temp;
-}
-/**
- *main- 
- *
- *Return: Always 0
- *
- */
-int main(void)
-{
-int x = 98;
-int y = 42;
-swap_int(&x, &y);
-return(0);
+*n = 98;
 }
